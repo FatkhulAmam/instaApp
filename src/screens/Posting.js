67a351fe848ff63d552f4ad57c346c3a0941f533
ahmad amam <ahmadfatkhul.amam@gmail.com>
2021-02-12 23:52:46 +0700

    styling profile

--- src/screens/Posting.js
+++ src/screens/Posting.js
@@ -1,17 +1,29 @@
 import React, {useEffect} from 'react';
-import {TouchableOpacity, Text, View} from 'react-native';
+import {TouchableOpacity, View, StyleSheet} from 'react-native';
+import {Text, Button} from 'native-base';
 
 const Posting = ({navigation}) => {
   useEffect(() => {
     navigation.navigate('EditPost');
   });
   return (
-    <View>
+    <View style={styles.parrent}>
       <TouchableOpacity onPress={navigation.goBack()}>
-        <Text>mmmm</Text>
+        <Text />
       </TouchableOpacity>
+      <Button onPress={() => navigation.navigate('EditPost')}>
+        <Text>add post</Text>
+      </Button>
     </View>
   );
 };
 
 export default Posting;
+
+const styles = StyleSheet.create({
+  parrent: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,14 +1,8 @@
 import React from 'react';
-import {
-  StyleSheet,
-  View,
-  StatusBar,
-  SafeAreaView,
-  TouchableOpacity,
-} from 'react-native';
-import {Header, Body, Right, Title} from 'native-base';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import {StyleSheet, View, StatusBar, SafeAreaView, Image} from 'react-native';
+import {Header, Body, Title, Text, Button} from 'native-base';
 import {useDispatch} from 'react-redux';
+import profile from '../assets/images/avatar.png';
 
 const Profile = () => {
   const dispatch = useDispatch();
@@ -24,12 +18,27 @@ const Profile = () => {
           <Body>
             <Title style={styles.text}>InstaApp</Title>
           </Body>
-          <Right>
-            <TouchableOpacity onPress={() => dispatch({type: 'LOGOUT'})}>
-              <Icon name="cog" size={25} style={styles.cogIcon} />
-            </TouchableOpacity>
-          </Right>
         </Header>
+      </View>
+      <View style={styles.parrent}>
+        <View style={styles.bio}>
+          <Image source={profile} style={styles.profile} />
+          <Text style={styles.postCount}>2 postingan</Text>
+        </View>
+        <Text style={styles.name}>Nama Pengguna</Text>
+        <Text style={styles.bioTxt}>
+          Biografi pengguna akan tertulis disini, maka pergunakan dengan baik
+        </Text>
+        <View style={styles.btnAction}>
+          <Button style={styles.btnEdit}>
+            <Text>Edit Profile</Text>
+          </Button>
+          <Button
+            style={styles.btnLogout}
+            onPress={() => dispatch({type: 'LOGOUT'})}>
+            <Text>logout</Text>
+          </Button>
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -38,14 +47,58 @@ const Profile = () => {
 export default Profile;
 
 const styles = StyleSheet.create({
+  header: {
+    backgroundColor: '#fff',
+  },
   text: {
     color: '#000000',
     marginLeft: 10,
     fontWeight: 'bold',
     fontSize: 25,
   },
+  postCount: {
+    fontSize: 25,
+    marginLeft: 50,
+  },
   cogIcon: {
     marginRight: 10,
     marginTop: 10,
+  },
+  parrent: {
+    padding: 15,
+    paddingRight: 25,
+    backgroundColor: '#fff',
+    borderBottomWidth: 1,
+    borderBottomColor: '#8e8e8e',
+  },
+  bio: {
+    flexDirection: 'row',
+    width: 230,
+    alignItems: 'center',
+  },
+  profile: {
+    width: 90,
+    height: 90,
+    borderRadius: 50,
+    marginRight: 10,
+  },
+  name: {
+    fontSize: 20,
+    marginTop: 10,
+    marginBottom: 5,
+  },
+  btnAction: {
+    flexDirection: 'row',
+    marginTop: 25,
+  },
+  btnEdit: {
+    marginBottom: 10,
+    marginRight: 10,
+    width: '50%',
+    justifyContent: 'center',
+  },
+  btnLogout: {
+    width: '50%',
+    justifyContent: 'center',
   },
 });
